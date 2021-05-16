0f16662e5d6192b18c6dfed7805c4c8df45f82b4
Rebecca DeBenedictis <rjdebened@verizon.net>
2021-05-16 13:21:27 -0400

    Initial Commit

---
+++ githubpractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  githubpractice
+//
+//  Created by Rebecca DeBenedictis on 5/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
